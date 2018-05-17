68bd4d2b50acba3da550b3c70bdc7fcd9b641619
Jose Toro <jose@leadbox.ca>
2018-05-17 01:34:09 -0300

    regular login and Google OAuth login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../services';
+import { User } from '../models/user.model';
 
 declare function load_plugins();
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -10,13 +14,52 @@ declare function load_plugins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  email: string;
+  rememberMe = false;
+  private auth2: any;
+
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
     load_plugins();
+    this.googleInit();
+
+    this.email = localStorage.getItem('email') || '';
+    if (this.email) {
+      this.rememberMe = true;
+    }
   }
 
-  login() {
-    this.router.navigate([ '/dashboard' ]);
+  login(form: NgForm) {
+    if (form.valid) {
+      let model = form.value;
+      let user = new User(null, model.email, model.password);
+      this.userService
+        .login(user, model.rememberMe)
+        .subscribe(() => this.router.navigate([ '/dashboard' ]));
+    }
+  }
+
+  private googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '376107001502-f0aa4if84887717pmcsrvibf56c9i8e4.apps.googleusercontent.com',
+        cookiepolicy: 'simple_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignin(document.getElementById('btnGoogle'));
+    });
+  }
+
+  private attachSignin(element) {
+    this.auth2.attachClickHandler(element, {}, googleUser => {
+      let token = googleUser.getAuthResponse().id_token;
+      this.userService
+        .googleLogin(token)
+        .subscribe(() => window.location.href = '#/dashboard');
+    });
   }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,30 +1,62 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import swal from 'sweetalert';
-import { map } from 'rxjs/operators';
+import { tap, map } from 'rxjs/operators';
 
 import { API } from './../../config';
 import { User } from '../../models/user.model';
 
-const USER_API = `${ API }/users`;
+const USER_URL = `${ API }/users`;
+const LOGIN_URL = `${ API }/login`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private user: User;
+  private token: string;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
 
+  login(user: User, rememberMe = false) {
+
+    if (rememberMe) {
+      localStorage.setItem('email', user.email);
+    } else {
+      localStorage.removeItem('email');
+    }
+
+    return this.http
+      .post(LOGIN_URL, user)
+      .pipe(
+        tap(this.saveSession),
+        map(_ => true)
+      );
+  }
+
+  googleLogin(token) {
+    return this.http
+      .post(`${ LOGIN_URL }/google`, { token })
+      .pipe(
+        tap(this.saveSession),
+        map(_ => true)
+      );
   }
 
   create(user: User) {
     return this.http
-      .post(USER_API, user)
+      .post(USER_URL, user)
       .pipe(
-        map(userCreated => {
-          swal('User Created', user.email, 'success');
-          return userCreated;
-        })
+        tap(() => swal('User Created', user.email, 'success'))
       );
+  }
+
+  private saveSession({ user, token }) {
+    localStorage.setItem('id', user._id);
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+
+    this.user = user;
+    this.token = token;
   }
 }
